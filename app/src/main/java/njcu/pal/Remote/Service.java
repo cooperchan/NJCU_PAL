673b2aec81f1ee093f82e74bcd01fa8a5298bc55
Yeury <jeffsuriel@gmail.com>
2017-07-25 12:06:20 -0400

    SignUpActivity updated.

--- app/src/main/java/njcu/pal/Remote/Service.java
+++ app/src/main/java/njcu/pal/Remote/Service.java
@@ -1,14 +1,18 @@
 package njcu.pal.Remote;
 
 import njcu.pal.LogInActivity;
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Yeury on 7/24/2017.
@@ -23,12 +27,20 @@ public interface Service {
     Call<Email> login(@Path("email")String email, @Path("password")String password);
 
 
+    @FormUrlEncoded
+    @POST("api/pal/signup")
+    Call <Email> signup(@Field("name") String name, @Field("email") String email, @Field("password") String password, @Field("school_code") String school_code, @Field("role") String role );
+
     class Factory {
         public static Service service;
 
         public static Service getInstance() {
             if (service == null) {
-                Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).build();
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .connectTimeout(100, TimeUnit.SECONDS)
+                        .readTimeout(100, TimeUnit.SECONDS).build();
+
+                Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).client(client).build();
                 service = retrofit.create(Service.class);
                 return service;
             } else {
--- app/src/main/java/njcu/pal/SignUpActivity.java
+++ app/src/main/java/njcu/pal/SignUpActivity.java
@@ -3,35 +3,95 @@ package njcu.pal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import njcu.pal.Remote.Email;
+import njcu.pal.Remote.Service;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     RadioButton student;
     RadioButton counselor;
     Button submit;
+    EditText name, email, password, school_code;
+
+    Integer rstudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        name = (EditText) findViewById(R.id.enter_Name);
+        email = (EditText) findViewById(R.id.enter_Email);
+        password = (EditText)findViewById(R.id.enter_Password);
+        school_code = (EditText) findViewById(R.id.enter_Code);
+
+
+
         student = (RadioButton) findViewById(R.id.signUp_student);
         counselor = (RadioButton) findViewById(R.id.signUp_counselor);
-        submit = (Button) findViewById(R.id.home_btn);
+        submit = (Button) findViewById(R.id.signUp_btn);
+
 
         student.setOnClickListener(this);
         counselor.setOnClickListener(this);
         submit.setOnClickListener(this);
-    }
+
+
+
+
+
+            }
+
+
 
     @Override
     public void onClick(View v) {
-
         switch(v.getId()) {
+
+            case R.id.signUp_student:
+                if (student.isChecked()) {
+                    rstudent = 1;
+                }
+                break;
+            case R.id.signUp_counselor:
+                if (counselor.isChecked()) {
+                    rstudent = 2;
+                }
+                break;
+            case R.id.signUp_btn:
+
+               // Integer school = Integer.parseInt(school_code.getText().toString());
+               // Integer student1 = Integer.parseInt(rstudent.toString());
+
+                Service.Factory.getInstance().signup("haha", "sdfa@haha.com", "passlol", "1234", "1").enqueue(new Callback<Email>() {
+                    @Override
+                    public void onResponse(Call<Email> call, Response<Email> response) {
+                        Log.d("Success", "Connection Successful");
+
+                        Email posts = response.body();
+                        Log.d("Error Code:", "" + posts.getStatus());
+                        Log.d("Success", posts.getMessage());
+                    }
+
+                    @Override
+                    public void onFailure(Call<Email> call, Throwable t) {
+                        Log.d("Failure", t.getLocalizedMessage());
+
+                    }
+                });
+        }
+
+      //  switch(v.getId()) {
 
             //case R.id.signUp_student:
 
@@ -45,4 +105,4 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                 //break;
         }
     }
-}
+
